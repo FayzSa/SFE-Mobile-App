4ebd168466ab82eda6603870f759f1fb09bff279
fayzsa <fayz.sabir@gmail.com>
2020-04-04 15:18:08 +0100

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: "SFE APP",
+    title: "SFE APP ",
     color: Colors.blue[200],
   ));
 
