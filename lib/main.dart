eefab2c28a2ed609c2a149aaf123d62c87b92b06
fayzsa <fayz.sabir@gmail.com>
2020-04-04 13:40:00 +0100

    intilize

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "SFE APP",
+    color: Colors.blue[200],
+  ));
 
-};
+}
 
